d662efe1ef3563b375b8e1765842db88a795fcfd
Joshua Luppes <cooljoebob64@gmail.com>
2021-02-02 08:52:10 -0600

    Add base class files with required fields and methods, and additional instruction doc

---
+++ src/main/java/com/tts/addressor/Addressor.java
@@ -0,0 +1,16 @@
+package com.tts.addressor;
+
+import java.util.ArrayList;
+
+public class Addressor {
+
+    private ArrayList<Entry> myList;
+
+    private void addEntry(){}
+    private void removeEntry(){}
+    private void searchEntries(){}
+    private void printAddressBook(){}
+    private void deleteAddressBook(){}
+    private void quit(){}
+
+}
---
+++ src/main/java/com/tts/addressor/Entry.java
@@ -0,0 +1,10 @@
+package com.tts.addressor;
+
+public class Entry {
+
+    private String nameFirst;
+    private String nameLast;
+    private String phoneNumber;
+    private String emailAddress;
+
+}
