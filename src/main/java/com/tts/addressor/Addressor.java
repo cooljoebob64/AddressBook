4ad9543c960b064699b6ecd27297b08af68e3c79
Joshua Luppes <cooljoebob64@gmail.com>
2021-02-02 10:46:12 -0600

    Add method body for removeEntry in Addressor class

--- src/main/java/com/tts/addressor/Addressor.java
+++ src/main/java/com/tts/addressor/Addressor.java
@@ -30,15 +30,41 @@ public class Addressor {
             throw new InputMismatchException("Email address already in book!");
         }
     }
-    public void removeEntry(String email){}
+
+    public void removeEntry(String email){
+        Entry searchEntry;
+        try{
+            searchEntry = searchEntries(email,SearchType.EMAIL).get(0);
+        } catch (NullPointerException e){
+            System.out.println("Error - No entry found: " + e);
+            return;
+        }
+        if(searchEntry!=null){
+            myList.remove(searchEntry);
+        }
+    }
+
     public ArrayList<Entry> searchEntries(String searchQuery){
         return myList;
     }
+
     public ArrayList<Entry> searchEntries(String searchQuery, SearchType searchType){
         return myList;
-    }
-    public void printAddressBook(){
 
     }
-    public void deleteAddressBook(){}
+
+    public void printAddressBook(){
+        if(myList.isEmpty()){
+            System.out.println("No entries in this address book!");
+        } else{
+            System.out.println("The contents of this address book:");
+            for(Entry entry:myList){
+                System.out.println(entry.toString());
+            }
+        }
+    }
+
+    public void deleteAddressBook(){
+        myList.clear();
+    }
 }
