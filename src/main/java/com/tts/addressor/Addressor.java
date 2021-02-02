68a8d1b8d7499ee8e00cc2c614563f775ca7d004
Joshua Luppes <cooljoebob64@gmail.com>
2021-02-02 09:43:05 -0600

    Add static factory method for Addressor class

--- src/main/java/com/tts/addressor/Addressor.java
+++ src/main/java/com/tts/addressor/Addressor.java
@@ -6,6 +6,12 @@ public class Addressor {
 
     private ArrayList<Entry> myList;
 
+    private Addressor(){}
+
+    public static Addressor createEmptyAddressor(){
+        return new Addressor();
+    }
+
     public void addEntry(){}
     public void removeEntry(){}
     public void searchEntries(){}
