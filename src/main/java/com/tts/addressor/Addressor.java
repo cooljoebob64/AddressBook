dabec2758923c5f0b3d0332abea4aab0465d7ea8
Joshua Luppes <cooljoebob64@gmail.com>
2021-02-02 10:34:22 -0600

    Add completed tests for Addressor class and update some return types of methods

--- src/main/java/com/tts/addressor/Addressor.java
+++ src/main/java/com/tts/addressor/Addressor.java
@@ -1,22 +1,44 @@
 package com.tts.addressor;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 
 public class Addressor {
 
     private ArrayList<Entry> myList;
 
-    private Addressor(){}
+    private Addressor(){
+        myList = new ArrayList<>();
+    }
+
+    public enum SearchType {
+        FIRSTNAME,
+        LASTNAME,
+        PHONE,
+        EMAIL;
+    }
 
     public static Addressor createEmptyAddressor(){
         return new Addressor();
     }
 
-    public void addEntry(Entry newEntry){}
-    public void removeEntry(Entry removedEntry){}
-    public void searchEntries(String searchQuery){}
-    public void printAddressBook(){}
-    public void deleteAddressBook(){}
-    public void quit(){}
+    public void addEntry (Entry newEntry)throws InputMismatchException{
+        ArrayList<Entry> searchResults = searchEntries(newEntry.getEmailAddress(), SearchType.EMAIL);
+        if(searchResults.isEmpty()){
+            myList.add(newEntry);
+        } else {
+            throw new InputMismatchException("Email address already in book!");
+        }
+    }
+    public void removeEntry(String email){}
+    public ArrayList<Entry> searchEntries(String searchQuery){
+        return myList;
+    }
+    public ArrayList<Entry> searchEntries(String searchQuery, SearchType searchType){
+        return myList;
+    }
+    public void printAddressBook(){
 
+    }
+    public void deleteAddressBook(){}
 }
--- src/test/java/com/tts/addressor/AddressorTest.java
+++ src/test/java/com/tts/addressor/AddressorTest.java
@@ -1,13 +1,50 @@
 package com.tts.addressor;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AddressorTest {
 
+    Addressor testAddressor;
+    Entry testEntry;
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+
+    @BeforeEach
+    void setUp() {
+        testAddressor = Addressor.createEmptyAddressor();
+        testEntry = Entry.createEntry(
+                "TestFirst",
+                "TestLast",
+                "TestPhone",
+                "TestEmail");
+    }
+
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+    }
+
     @Test
-    void createEmptyAddressor(){
+    void createEmptyAddressor() {
         Addressor newAddressor = Addressor.createEmptyAddressor();
 //        assertTrue(newAddressor instanceof Addressor);
         assertNotEquals(newAddressor, null);
@@ -15,26 +52,50 @@ class AddressorTest {
 
     @Test
     void addEntry() {
+        testAddressor.addEntry(testEntry);
+        ArrayList<Entry> searchResult = testAddressor.searchEntries(testEntry.getEmailAddress(), Addressor.SearchType.EMAIL);
 
+        String expected = testEntry.getEmailAddress();
+        String actual = searchResult.get(0).getEmailAddress();
+        assertTrue(actual.contains(expected));
     }
 
     @Test
     void removeEntry() {
+        testAddressor.addEntry(testEntry);
+        testAddressor.removeEntry(testEntry.getEmailAddress());
+        ArrayList<Entry> searchResult = testAddressor.searchEntries(testEntry.getEmailAddress(), Addressor.SearchType.EMAIL);
+
+        boolean expected = true;
+        boolean actual = searchResult.isEmpty();
+        assertEquals(expected, actual);
     }
 
     @Test
     void searchEntries() {
+        testAddressor.addEntry(testEntry);
+        ArrayList<Entry> searchResult = testAddressor.searchEntries(testEntry.getEmailAddress(), Addressor.SearchType.EMAIL);
+
+        String expected = testEntry.getEmailAddress();
+        String actual = searchResult.get(0).getEmailAddress();
+        assertTrue(actual.contains(expected));
     }
 
     @Test
     void printAddressBook() {
+        testAddressor.printAddressBook();
+        assertNotEquals(null, outContent.toString());
     }
 
     @Test
     void deleteAddressBook() {
-    }
+        testAddressor.addEntry(testEntry);
+        testAddressor.deleteAddressBook();
 
-    @Test
-    void quit() {
+        ArrayList<Entry> searchResult = testAddressor.searchEntries(testEntry.getEmailAddress(), Addressor.SearchType.EMAIL);
+
+        boolean expected = true;
+        boolean actual = searchResult.isEmpty();
+        assertEquals(expected,actual);
     }
 }
